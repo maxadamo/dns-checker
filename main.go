7001d3fa5c491f94dfc85a8b57b41fc0f3e58604
Massimiliano Adamo <maxadamo@gmail.com>
2021-11-05 01:11:26 +0100

    added Log Warning

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/docopt/docopt-go"
 )
 
+// if the below HTML snippet changes a new MD5 digest code has to be used from LVS
 var statusTemplate, _ = template.New("status template").Parse(`<!DOCTYPE html>
 <html>
   <head>
@@ -100,29 +101,33 @@ func checkDNS(proto string, DNSport string, consulPort string, recordDNS string,
 	return "DNS is UP"
 }
 
+// serve directory ipv4
 func ipv4(w http.ResponseWriter, req *http.Request) {
 	ipv4_dns_status := checkDNS("ipv4", dnsPort, consulPort, dnsRecord, consulRecord, consulBool)
-	if verbose {
-		InfoLogger.Println(ipv4_dns_status)
-	}
 	if ipv4_dns_status == "DNS is UP" {
+		if verbose {
+			InfoLogger.Println(ipv4_dns_status)
+		}
 		w.WriteHeader(http.StatusOK)
 		statusTemplate.Execute(w, template.HTML(ipv4_dns_status))
 	} else {
+		WarningLogger.Println(ipv4_dns_status)
 		w.WriteHeader(http.StatusServiceUnavailable)
 		statusTemplate.Execute(w, template.HTML(ipv4_dns_status))
 	}
 }
 
+// serve directory ipv6
 func ipv6(w http.ResponseWriter, req *http.Request) {
 	ipv6_dns_status := checkDNS("ipv6", dnsPort, consulPort, dnsRecord, consulRecord, consulBool)
-	if verbose {
-		InfoLogger.Println(ipv6_dns_status)
-	}
 	if ipv6_dns_status == "DNS is UP" {
+		if verbose {
+			InfoLogger.Println(ipv6_dns_status)
+		}
 		w.WriteHeader(http.StatusOK)
 		statusTemplate.Execute(w, template.HTML(ipv6_dns_status))
 	} else {
+		WarningLogger.Println(ipv6_dns_status)
 		w.WriteHeader(http.StatusServiceUnavailable)
 		statusTemplate.Execute(w, template.HTML(ipv6_dns_status))
 	}
@@ -136,8 +141,7 @@ func main() {
   - checks DNS and optionally Consul and report the status on a Web page
   
 Usage:
-  %v [--dns-port=DNSPORT] [--consul-port=CONSULPORT] [--dns-record=DNSRECORD] [--consul-record=CONSULRECORD] [--consul] [--verbose] [--ipv6] [--listen-port=LISTENPORT]
-
+  %v [--dns-port=DNSPORT] [--consul-port=CONSULPORT] --dns-record=DNSRECORD [--consul-record=CONSULRECORD] [--consul] [--verbose] [--listen-port=LISTENPORT]
   %v -h | --help
   %v -v | --version
   %v -b | --build
@@ -148,12 +152,11 @@ Options:
   -b --build                        Print version and build information and exit
   --dns-port=DNSPORT                DNS port [default: 53]
   --consul-port=CONSULPORT          Consul port [default: 8600]
-  --dns-record=DNSRECORD            DNS record to check [default: www.geant.org]
+  --dns-record=DNSRECORD            DNS record to check. A local record is recommended.
   --consul-record=CONSULRECORD      Consul record to check [default: consul.service.consul]
   --consul                          Check consul DNS as well
-  --ipv6                            Check IPv6 too
   --listen-port=LISTENPORT          Web server port [default: 10053]
-  --verbose                         Log connections
+  --verbose                         Log also successful connections
 `, progName, progName, progName, progName)
 
 	arguments, _ := docopt.ParseArgs(usage, nil, appVersion)
@@ -180,7 +183,7 @@ Options:
 	consulRecord = arguments["--consul-record"].(string)
 
 	http.HandleFunc("/ipv4", ipv4)
-	http.HandleFunc("/ipv6", ipv6)
+	http.HandleFunc("/ipv6", ipv6) // IPv6 can be left on by default. If not needed it won't be used.
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", listenPort), nil))
 
